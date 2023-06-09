9a846e4e929c02d458993679d105b599470175df
Mohammed-Abdo-Edries <mohammed.abdo1916@gmail.com>
2023-06-09 11:25:24 -0700

    url added

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,1 @@
 export const url = "https://localhost:4000";
-export const setHeaders = () => {
-    const headers = {
-        headers:{
-            "Content-type": "application/json",
-            "token": localStorage.getItem("token")
-        }
-    };
-    return headers;
-}
